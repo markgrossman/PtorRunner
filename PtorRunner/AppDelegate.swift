3eec46592ee32d1c72b77a0eca2b5733c70b6021
mark.grossman <mark.grossman@servicenow.com>
2014-10-17 17:12:14 -0700

    Initial Commit

---
+++ PtorRunner/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PtorRunner
+//
+//  Created by Mark Grossman on 10/17/14.
+//  Copyright (c) 2014 Mark Grossman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
